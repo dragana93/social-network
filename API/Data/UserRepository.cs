fff1571e2901d9ff8085ced21cc705b4dc448ad1
draganasavanovic <dragana.savanovic@wayseven.com>
2022-02-18 11:58:42 +0100

    added helper classes for pagination and used the pagination classes

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -1,5 +1,6 @@
 ﻿using API.DTOs;
 using API.Entities;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
@@ -21,11 +22,13 @@ namespace API.Data
             _mapper = mapper;
         }
 
-        public async Task<IEnumerable<MemberDto>> GetMembersAsync()
+        public async Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams)
         {
-            return await _context.Users
+            var query = _context.Users
               .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
-              .ToListAsync();
+              .AsNoTracking();
+
+            return await PagedList<MemberDto>.CreateAsync(query, userParams.PageNumber, userParams.PageSize);
         }
 
         public async Task<MemberDto> GetMemberAsync(string username)
