f201df2905dfaee5be26f321eff392a60702ff4d
draganasavanovic <dragana.savanovic@wayseven.com>
2022-02-22 15:08:03 +0100

    setting up client pagination

--- client/src/app/services/members.service.ts
+++ client/src/app/services/members.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Member } from '../models/member';
 import { map } from 'rxjs/operators';
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { PaginatedResult } from '../models/pagination';
 
 // const httpOptions =  {
 //   headers: new HttpHeaders({
@@ -17,16 +18,23 @@ import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 export class MembersService {
   baseUrl = environment.apiUrl;
   members: Member[] = [];
+  paginatedResult: PaginatedResult<Member[]> = new PaginatedResult<Member[]>();
 
   constructor(private http: HttpClient) { }
 
-  getMembers(): Observable<Member[]>{
-
-    if(this.members.length > 0) return of(this.members);
-    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
-      map(members => {
-        this.members = members;
-        return members;
+  getMembers(page?: number, itemsPerPage?: number){
+    let params = new HttpParams();
+    if(page !== null && itemsPerPage !== null){
+      params = params.append('pageNumber', page.toString());
+      params = params.append('pageSize', itemsPerPage.toString());
+    }
+    return this.http.get<Member[]>(this.baseUrl + 'users', {observe: 'response', params}).pipe(
+      map(response => {
+       this.paginatedResult.result = response.body;
+       if(response.headers.get('Pagination') !== null){
+         this.paginatedResult.pagination = JSON.parse(response.headers.get('Pagination'))
+       }
+       return this.paginatedResult;
       })
     );
   }
