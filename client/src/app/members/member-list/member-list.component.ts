8512faeceb8959da03b93d287b468d79a8ed7554
draganasavanovic <dragana.savanovic@wayseven.com>
2022-02-28 15:57:41 +0100

    added filter buttons to the client

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -18,28 +18,38 @@ export class MemberListComponent implements OnInit {
   pagination: Pagination;
   userParams: UserParams;
   user: User;
+  genderList = [
+    { value: 'male', display: 'Males' },
+    { value: 'female', display: 'Females' },
+  ];
 
-  constructor(private memberService: MembersService, private accountService: AccountService) {
-    this.accountService.currentUser$.pipe(take(1)).subscribe(user => {
+  constructor(
+    private memberService: MembersService,
+    private accountService: AccountService
+  ) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe((user) => {
       this.user = user;
       this.userParams = new UserParams(user);
-    })
+    });
   }
 
   ngOnInit(): void {
-   this.loadMembers();
+    this.loadMembers();
   }
 
-  loadMembers(){
-    this.memberService.getMembers(this.userParams).subscribe(
-      response =>{
-        this.members = response.result;
-        this.pagination = response.pagination;
-      }
-    );
+  loadMembers() {
+    this.memberService.getMembers(this.userParams).subscribe((response) => {
+      this.members = response.result;
+      this.pagination = response.pagination;
+    });
   }
 
-  pageChanged(event: any){
+  resetFilters(){
+    this.userParams = new UserParams(this.user);
+    this.loadMembers();
+  }
+
+  pageChanged(event: any) {
     this.userParams.pageNumber = event.page;
     this.loadMembers();
   }
