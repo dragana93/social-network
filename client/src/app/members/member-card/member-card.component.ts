5aad9dbed62551d6765e601e10acfde3755edc31
draganasavanovic <dragana.savanovic@wayseven.com>
2022-03-07 09:11:52 +0100

    setted up the likes functions in the angular app

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Member } from '../../models/member';
+import { MembersService } from 'src/app/services/members.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-member-card',
@@ -10,9 +12,15 @@ export class MemberCardComponent implements OnInit {
 
   @Input() member: Member;
 
-  constructor() { }
+  constructor(private memberService: MembersService, private toaster: ToastrService) { }
 
   ngOnInit(): void {
+  }
+
+  addLike(member: Member){
+    this.memberService.addLike(member.username).subscribe(() => {
+      this.toaster.success('You have liked ' + member.knownAs);
+    });
   }
 
 }
