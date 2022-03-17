fb814d2f4971f28fe6a9a30aec8ac70477ea1bad
draganasavanovic <dragana.savanovic@wayseven.com>
2022-03-17 09:40:29 +0100

    setted up the angular app for messaging

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from '../models/message';
+import { Pagination } from '../models/pagination';
+import { MessageService } from '../services/message.service';
 
 @Component({
   selector: 'app-messages',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessagesComponent implements OnInit {
 
-  constructor() { }
+  messages: Message[];
+  pagination: Pagination;
+  container = 'Outbox';
+  pageNumber = 1;
+  pageSize = 5;
+
+  constructor(private messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.loadMessages();
   }
 
+  loadMessages(){
+    this.messageService.getMessages(this.pageNumber, this.pageSize, this.container).subscribe(respone => {
+      this.messages = respone.result;
+      this.pagination = respone.pagination;
+    });
+  }
+
+  pageChanged(event: any){
+    if(this.pageNumber !== event.page){
+      this.pageNumber = event.page;
+      this.loadMessages();
+    }
+  }
 }
---
+++ client/src/app/services/message.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { getPaginationHeaders, getPaginatedResult } from './paginationHelper';
+import { Message } from '../models/message';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getMessages(pageNumber, pageSize, container){
+    let params = getPaginationHeaders(pageNumber, pageSize);
+    params = params.append('Container', container);
+    return getPaginatedResult<Message[]>(this.baseUrl + 'messages', params, this.http);
+  }
+
+
+
+}
